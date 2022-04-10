edcb0598a493928e58d5a3a23bc908c3500ea135
khalifa <ibrahim.idris.developer@gmail.com>
2022-04-10 21:33:19 +0100

    update useD3 logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import BarChart from './BarChart';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    
+    fetch(`http://api.worldbank.org/v2/countries/GB/indicators/SP.POP.TOTL?per_page=5000&format=json`)
+    .then(response => response.json())
+    .then(result => {
+      let sortedData = result[1].sort((a, b) => +a.date - +b.date).map((entry) => ({...entry, date: +entry.date}));
+      setData(sortedData);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <p>Hello world!</p>
+      {
+        data.length === 0 ? <p>Loading data ...</p> : <BarChart data={data} />
+      }
     </div>
   );
 }
